46a446997d69be06c141d21adadd112e0944b27d
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2023-11-13 00:54:26 -0300

    caracAnimais refatorado

--- Ex_Modulo9 (Laços-Decisao)/L1_Ex4_CaracAnimais.java
+++ Ex_Modulo9 (Laços-Decisao)/L1_Ex4_CaracAnimais.java
@@ -4,56 +4,84 @@ import java.util.Scanner;
 
 public class L1_Ex4_CaracAnimais {
 	
+	enum TipoCaracteristicas {
+		VERTEBRADO,
+        INVERTEBRADO,
+        AVE,
+        MAMIFERO,
+        INSETO,
+        ANELIDEO,
+        CARNIVORO,
+        ONIVORO,
+        HERBIVORO,
+        HEMATOFAGO
+	}
+	
 	public static void main(String[] args) {
 		
 		Scanner read = new Scanner(System.in);
 		
-		String carac1, carac2, carac3;
-		
-		System.out.println("Digite a primeira característica: ");
-		carac1 = read.next();
-		System.out.println("Digite a segunda característica: ");
-		carac2 = read.next();
-		System.out.println("Digite a terceira característica: ");
-		carac3 = read.next();
-		
-		if(carac1.equalsIgnoreCase("Vertebrado")) {
-			if(carac2.equalsIgnoreCase("Ave")) {
-				if(carac3.equalsIgnoreCase("Carnívoro")) {
-					System.out.println("Águia");
-				} else if (carac3.equalsIgnoreCase("Onívoro")) {
-					System.out.println("Pomba");
-				} else {
-					System.out.println("Inválido");
-				}
-			} else if (carac2.equalsIgnoreCase("Mamífero")){
-				if(carac3.equalsIgnoreCase("Onívoro")) {
-					System.out.println("Homem");
-				} else if (carac3.equalsIgnoreCase("Herbívoro")) {
-					System.out.println("Vaca");
-				} else {
-					System.out.println("Inválido");
-				}
-			}
-		} else if (carac1.equalsIgnoreCase("Invertebrado")) {
-			if(carac2.equalsIgnoreCase("Inseto")) {
-				if(carac3.equalsIgnoreCase("Hematófago")) {
-					System.out.println("Pulga");
-				} else if (carac3.equalsIgnoreCase("Herbívoro")) {
-					System.out.println("Lagarta");
-				} else {
-					System.out.println("Inválido");
-				}
-			} else if (carac2.equalsIgnoreCase("Anelídeo")){
-				if(carac3.equalsIgnoreCase("Hematófago")) {
-					System.out.println("Sanguessuga");
-				} else if (carac3.equalsIgnoreCase("Onívoro")) {
-					System.out.println("Minhoca");
-				} else {
-					System.out.println("Inválido");
-				}
-			}
-		}
+		TipoCaracteristicas carac1 = lerCaracteristica(read, "Digite a primeira característica: ");
+        TipoCaracteristicas carac2 = lerCaracteristica(read, "Digite a segunda característica: ");
+        TipoCaracteristicas carac3 = lerCaracteristica(read, "Digite a terceira característica: ");
+        
+        determinarAnimal(carac1,carac2,carac3);
 		
 	}
+	
+	private static TipoCaracteristicas lerCaracteristica(Scanner read, String mensagem) {
+        System.out.println(mensagem);
+        String input = read.next().toUpperCase();
+        return TipoCaracteristicas.valueOf(input);
+    }
+	
+	private static void determinarAnimal(TipoCaracteristicas carac1, TipoCaracteristicas carac2, TipoCaracteristicas carac3) {
+        if (carac1 == TipoCaracteristicas.VERTEBRADO) {
+            if (carac2 == TipoCaracteristicas.AVE) {
+                if (carac3 == TipoCaracteristicas.CARNIVORO) {
+                    System.out.println("Águia");
+                } else if (carac3 == TipoCaracteristicas.ONIVORO) {
+                    System.out.println("Pomba");
+                } else {
+                    animalInvalido();
+                }
+            } else if (carac2 == TipoCaracteristicas.MAMIFERO) {
+                if (carac3 == TipoCaracteristicas.ONIVORO) {
+                    System.out.println("Homem");
+                } else if (carac3 == TipoCaracteristicas.HERBIVORO) {
+                    System.out.println("Vaca");
+                } else {
+                    animalInvalido();
+                }
+            } else {
+                animalInvalido();
+            }
+        } else if (carac1 == TipoCaracteristicas.INVERTEBRADO) {
+            if (carac2 == TipoCaracteristicas.INSETO) {
+                if (carac3 == TipoCaracteristicas.HEMATOFAGO) {
+                    System.out.println("Pulga");
+                } else if (carac3 == TipoCaracteristicas.HERBIVORO) {
+                    System.out.println("Lagarta");
+                } else {
+                    animalInvalido();
+                }
+            } else if (carac2 == TipoCaracteristicas.ANELIDEO) {
+                if (carac3 == TipoCaracteristicas.HEMATOFAGO) {
+                    System.out.println("Sanguessuga");
+                } else if (carac3 == TipoCaracteristicas.ONIVORO) {
+                    System.out.println("Minhoca");
+                } else {
+                    animalInvalido();
+                }
+            } else {
+                animalInvalido();
+            }
+        } else {
+            animalInvalido();
+        }
+    }
+
+    private static void animalInvalido() {
+        System.out.println("Inválido");
+    }
 }
