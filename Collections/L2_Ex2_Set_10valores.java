5e56fb6a8f3a7ccfacec81158f01cdfdf4d7f2e8
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2023-11-14 13:54:46 -0300

    collec-1.0

--- Collections/L2_Ex2_Set_10valores.java
+++ Collections/L2_Ex2_Set_10valores.java
@@ -18,24 +18,31 @@ public class L2_Ex2_Set_10valores {
 		for(int i = 0; i < 10; i++) {
 			int valor;
 			
+			//Lê o numero digitado
 			do {
 				System.out.println("Valor "+(1 + i)+": ");
 				valor = read.nextInt();
+				// verifica se o número já foi digitado antes
 				if(listaNum.contains(valor)) {
 					System.out.println("Esse número já foi digitado. ");
 				}
+				//retorna os numeros para fora do loop
 			} while (listaNum.contains(valor));
 			
+			//adiciona o numero digitado para cada indice
 			listaNum.add(valor);
 		}
 		
 		System.out.println("\nValores digitados:");
-        Iterator<Integer> iterator = listaNum.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+		
+		//Instancia a classe Iterator
+        Iterator<Integer> listaNumInt = listaNum.iterator();
+        
+        //Loop para imprimir o array na tela (hasNext() torna os valores booleanos, verificando se existe o proximo valor. Quando não existe, ele retorna false)
+        while (listaNumInt.hasNext()) {
+        	//Impressão dos número (next() atua como contador para o loop)
+            System.out.println(listaNumInt.next());
         }
-
-        read.close();
 	}
 
 }
